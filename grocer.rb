f1be31916ff26cc88ab26097a04a27e25a0b5d97
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-23 16:38:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -88,6 +88,7 @@ end
 def apply_clearance(cart)
   i = 0
   while i < cart.length do
+    
     if cart[i][:clearance]
       cart[i][:price] = (cart[i][:price] - (cart[i][:price] * 0.20)).round(2)
     end
