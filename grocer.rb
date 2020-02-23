2b97bfc685612e06685b5a1bb7f890923345d366
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-23 16:12:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,7 +53,6 @@ def apply_coupons(cart, coupons)
 # i = 0
 #   while coupons[i] < coupons.length do 
 #     item = find_item_by_name_in_collection(coupons[i][:item], cart) 
-
 #     coupon_item = "#{coupons[i][:item]} W/COUPON"
 #     item_w_coupon = find_item_by_name_in_collection(coupon_item, cart)
 
